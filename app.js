d53287f47ff1d98696cac54dc11fab10bb968df6
Viorel Stefan <iviostefan@gmail.com>
2021-01-05 16:43:24 +0200

    start menu - frame

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+    let start = document.getElementById("start");
+
+    let start_menu = document.getElementById("start-menu");
+
+    start.addEventListener("click", function() {
+        if(start_menu.style.display == "none") {
+            start_menu.style.display = "block";
+        } else {
+            start_menu.style.display = "none";
+        }
+        
+    })
 
 })
