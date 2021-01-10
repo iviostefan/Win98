450e33cc011ca707709a174bf744a272c798d945
Viorel Stefan <iviostefan@gmail.com>
2021-01-10 13:51:01 +0200

    boot

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const bootwin = () => {
+        document.getElementById("boot").style.display = "none";
+        document.getElementById("win98").style.display = "block";
+    }
+    //setTimeout(bootwin, 2000);
+    bootwin();
     const fill = (number) => number < 10 ? number = "0" + number : number;
     const getTime = () => {
         let date = new Date();
         document.getElementById("taskbar-notification").innerHTML = fill(date.getHours()) + ":" + fill(date.getMinutes());
     }
-
-    const newLocal = setInterval(getTime, 100);
-    let desktop = document.getElementsByTagName("body")[0];
+    setInterval(getTime, 100);
+    let desktop = document.getElementById("win98");
     let start = document.getElementById("start");
     let taskbar = document.getElementById("start-bar");
     let start_menu = document.getElementById("start-menu");
     let task_icons = document.getElementById("taskbar-icons");
-
     start.addEventListener("click", function () {
         if (start_menu.style.display == "none") {
             start_menu.style.display = "block";
@@ -20,7 +24,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
     })
-
     class Window {
         constructor(title, type, img) {
             this.title = title;
@@ -31,80 +34,65 @@ document.addEventListener("DOMContentLoaded", function () {
             let window = document.createElement("div");
             window.setAttribute("class", "window");
             window.setAttribute("id", this.type);
-
+            let header = document.createElement("div");
+            let header_img = document.createElement("img");
+            header.setAttribute("class", "window-header");
+            header_img.setAttribute("src", this.img);
             let wind_adress = document.createElement("div");
             wind_adress.setAttribute("class", "window-address");
             window.appendChild(wind_adress);
             let wind_content = document.createElement("div");
             wind_content.setAttribute("class", "wind-content");
+            let imgw = document.createElement("img");
+            imgw.setAttribute("class", "backg-mycom");
+            let list_icons = document.createElement("div");
+            list_icons.setAttribute("class", "icons-comp");
+            switch (this.type) {
+                case "My Computer":
+                    imgw.setAttribute("src", "https://i.ibb.co/2S75bpY/mycompf.png");
+                    let flop = document.createElement("img");
+                    flop.setAttribute("src", "https://i.ibb.co/WkSxgbm/mycomff.png");
+                    list_icons.appendChild(flop);
+                    let hdd = document.createElement("img");
+                    hdd.setAttribute("src", "https://i.ibb.co/8Dfv2Vy/comfc.png");
+                    list_icons.appendChild(hdd);
+                    let cd = document.createElement("img");
+                    cd.setAttribute("src", "https://i.ibb.co/mq7c1jn/compfcd.png");
+                    list_icons.appendChild(cd);
+                    let cp = document.createElement("img");
+                    cp.setAttribute("src", "https://i.ibb.co/28PBBdN/compfcp.png");
+                    list_icons.appendChild(cp);
+                    break;
+                case "My Documents":
+                    imgw.setAttribute("src", "https://i.ibb.co/MRvPPm4/mydocf.png");
+                    let folder = document.createElement("img");
+                    folder.setAttribute("src", "https://i.ibb.co/bJSCHnV/mypicico.png");
+                    folder.addEventListener("click", function() {
+                        folder.style.display = "none";
+                        let imgs = document.createElement("div");
+                        imgs.setAttribute("class", "pictures");
+                        let img1 = document.createElement("div");
+                        img1.setAttribute("class", "thumb1");
+                        let img2 = document.createElement("div");
+                        img2.setAttribute("class", "thumb2");
+                        imgs.appendChild(img1);
+                       imgs.appendChild(img2);
+                       list_icons.appendChild(imgs);
 
-            if (this.type === "My Computer") {
-                let imgw = document.createElement("img");
-                imgw.setAttribute("class", "backg-mycom");
-                imgw.setAttribute("src", "https://i.ibb.co/2S75bpY/mycompf.png");
-                wind_content.appendChild(imgw);
-
-                let list_icons = document.createElement("div");
-                list_icons.setAttribute("class", "icons-comp");
-
-                let flop = document.createElement("img");
-                flop.setAttribute("src", "https://i.ibb.co/WkSxgbm/mycomff.png");
-                list_icons.appendChild(flop);
-
-                let hdd = document.createElement("img");
-                hdd.setAttribute("src", "https://i.ibb.co/8Dfv2Vy/comfc.png");
-                list_icons.appendChild(hdd);
-
-                let cd = document.createElement("img");
-                cd.setAttribute("src", "https://i.ibb.co/mq7c1jn/compfcd.png");
-                list_icons.appendChild(cd);
-
-                let cp = document.createElement("img");
-                cp.setAttribute("src", "https://i.ibb.co/28PBBdN/compfcp.png");
-                list_icons.appendChild(cp);
-
-                wind_content.appendChild(list_icons);
-
-                window.appendChild(wind_content);
-
+                    })
+                    list_icons.appendChild(folder);
+                    break;
+                case "Internet Explorer":
+                    break;
+                case "Network":
+                    imgw.setAttribute("src", "https://i.ibb.co/TMr2G92/cpfnet.png");
+                    break;
+                case "Recycle Bin":
+                    break;
             }
-
-            if (this.type === "My Documntes") {
-                let imgw = document.createElement("img");
-                imgw.setAttribute("src", "https://i.ibb.co/MRvPPm4/mydocf.png");
-                wind_content.appendChild(imgw);
-
-                let list_icons = document.createElement("div");
-                list_icons.setAttribute("class", "icons-comp");
-
-                let folder = document.createElement("img");
-                folder.setAttribute("src", "https://i.ibb.co/bJSCHnV/mypicico.png");
-                list_icons.appendChild(folder);
-                wind_content.appendChild(list_icons);
-
-                window.appendChild(wind_content);
-            }
-
-            if (this.type === "Internet Explorer") {
-
-            }
-
-            if (this.type === "Network") {
-                let imgw = document.createElement("img");
-                imgw.setAttribute("src", "https://i.ibb.co/TMr2G92/cpfnet.png");
-                wind_content.appendChild(imgw);
-            }
-
-            if (this.type === "Recycle Bin") {
-
-            }
-
-
-
-            let header = document.createElement("div");
-            let header_img = document.createElement("img");
-            header.setAttribute("class", "window-header");
-            header_img.setAttribute("src", this.img);
+            wind_content.appendChild(imgw);
+            wind_content.appendChild(list_icons);
+            window.appendChild(wind_content);
 
             let task = document.createElement("div");
             task.setAttribute("class", "task");
@@ -118,9 +106,6 @@ document.addEventListener("DOMContentLoaded", function () {
                     window.classList.remove("zindex");
 
                 }
-                //   window.classList.toggle("minimize");
-
-
             })
             let task_img = document.createElement("img");
             task_img.setAttribute("src", this.img)
@@ -203,9 +188,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // let win = new Window();
-    // win.open();
-
     class Icon {
         constructor(name, logo) {
             this.name = name;
@@ -221,8 +203,6 @@ document.addEventListener("DOMContentLoaded", function () {
             div.addEventListener("click", function () {
                 let win = new Window(p.innerHTML, p.innerHTML, img.src);
                 win.open();
-
-
             })
             div.appendChild(img);
             div.appendChild(p);
